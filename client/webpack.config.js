41f1fc375a86bb7f39f87337df89d87faee9a8bb
svcosta <sergio.@hotmail.com>
2018-02-17 11:16:45 +0000

    plugin css optimioptimizeCSSAssetsPlugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,12 +1,31 @@
 const path = require('path'); //pega o caminho absolutpe
 const babiliPlugin = require('babili-webpack-plugin');
+//plugin para separar o javacript do css
+const extractTextPlugin = require('extract-text-webpack-plugin'); 
+//plugin para optimizar o css
+const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
 
 //lista de plugins
 let plugins = [];
 
+plugins.push(
+    new extractTextPlugin("styles.css")
+);
+
 //se for ambiente de producao 
 if(process.env.NODE_ENV == 'production') {
     plugins.push(new babiliPlugin());
+
+    plugins.push(new optimizeCSSAssetsPlugin({
+        cssProcessor: require('cssnano'),
+        cssProcessorOptions: { 
+            discardComments: {
+                removeAll: true 
+            }
+        },
+        canPrint: true
+     }));   
 }
 
 module.exports = {
@@ -25,10 +44,17 @@ module.exports = {
                         loader:'babel-loader'
                     }
             } ,
-            { 
-                test: /\.css$/, 
-                loader: 'style-loader!css-loader' //executa da direita para esquerda exe -> 1) css-loader 2)tyle-loader 
-            } ,
+            // { 
+            //     test: /\.css$/, 
+            //     loader: 'style-loader!css-loader' //executa da direita para esquerda exe -> 1) css-loader 2)tyle-loader 
+            // } ,
+            {
+                test: /\.css$/,
+                use: extractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                }) //ira separar o css do javascript
+            },
             {  ///Loader font
                 test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
                 loader: 'url-loader?limit=10000&mimetype=application/font-woff' 
@@ -68,6 +94,18 @@ module.exports = {
  * 
  * Loader font
  * npm install url-loader@0.5.9 file-loader@0.11.2 --save-dev
+ * 
+ * css estractor
+ * npm install extract-text-webpack-plugin@3.0.0 --save-dev
+ * 
+ * 
+ *  optimize-css-assets-webpack-plugin: Ele receberá a responsabilidade por processar o style.css.
+ *  gerado antes de ser gravado na pasta de build. Porém, ele nao sabe processar o CSS, ainda será 
+ * preciso ter um minificador CSS, no caso, usaremos o cssnano. 
+ * . Podemos combinar os dois plugin com Webpack, juntamente com cssnano, que pode ser usado em qualquer 
+ * sistema. Faremos a instalação pelo Terminal:
+ * npm install optimize-css-assets-webpack-plugin@2.0.0 --save-dev
+   npm install cssnano@3.10.0 --save-dev
  * 
  * npm run build
  */
